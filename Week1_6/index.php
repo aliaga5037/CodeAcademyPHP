ced9646d9aa746580dc7872d57264fc6af102ad5
nanophp <nanophp@hotmail.com>
2016-06-09 07:45:58 -0700

    update

--- Week1_6/index.php
+++ Week1_6/index.php
@@ -1,14 +1,24 @@
 <?php
-
-    class Person {
-        protected $name = "Valeh";
-    }
-
-    class Human extends Person {
-        public function getName() {
-            return $this->name;
+    class myClass {
+        public $login;
+        protected $pass;
+        public function __construct()
+        {
+            echo "The first our constructor work in <b>" . __CLASS__ . "</b><br>";
+        }
+        public function addUser($user, $pass) {
+            $this->login = $user;
+            $this->pass = $pass;
         }
     }
 
-    $student = new Human;
-    echo $student->getName();
+    class userPanel extends myClass {
+        public function showUser() {
+            echo "Username: " . $this->login . "<br>";
+            echo "Password: " . $this->pass . "<br>"; //error
+        }
+    }
+
+    $panel = new userPanel;
+    $panel->addUser("root", "userpassword");
+    $panel->showUser();
